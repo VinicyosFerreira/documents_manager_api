ad1ea3f689de1c16147f745185885e0157d40101
Vinicyos Ferreira <vinifr987@gmail.com>
2026-03-04 12:08:13 -0300

    chore: add setup typescript, prettier and eslint

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+    },
+  },
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+]);
