c1db75162ac2dd19ea2ffd71fd104e5758fce090
Vinicyos Ferreira <vinifr987@gmail.com>
2026-03-04 19:25:43 -0300

    chore: add CreateDocumentInputDTO and CreateDocumentOutputDTO

---
+++ src/dtos/document/create-document-dto.ts
@@ -0,0 +1,14 @@
+import { Status } from "../../../generated/prisma/enums.js";
+
+export interface CreateDocumentInputDTO {
+  titulo: string;
+  descricao: string;
+}
+
+export interface CreateDocumentOutputDTO {
+  id: string;
+  titulo: string;
+  descricao: string;
+  status: Status;
+  criado_em: Date;
+}
---
+++ src/dtos/index.ts
@@ -0,0 +1,1 @@
+export * from './document/create-document-dto.js'
