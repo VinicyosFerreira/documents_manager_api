96f6d4642debd22c32138f3b0471cf88d40f24e7
Vinicyos Ferreira <vinifr987@gmail.com>
2026-03-04 12:14:31 -0300

    chore: add tsx

--- index.ts
+++ index.ts
@@ -1,2 +1,16 @@
-const teste = 10;
-console.log(teste);
+import Fastify from 'fastify';
+
+const app = Fastify();
+
+app.get('/', async () => {
+  return { hello: 'world' };
+});
+
+try {
+  await app.listen({
+    port: 8000,
+  });
+} catch (err) {
+  app.log.error(err);
+  process.exit(1);
+}
