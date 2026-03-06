eda97ff265fedf2f4b53640b5fe817bf3f80e397
Vinicyos Ferreira <vinifr987@gmail.com>
2026-03-06 14:59:19 -0300

    fix: correction in CreateDocumentSchema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,7 +1,7 @@
 import z from 'zod';
 
 export const CreateDocumentSchema = z.object({
-  titulo: z.string({}).trim().min(1, {
+  titulo: z.string().trim().min(1, {
     message: 'O título é obrigatório',
   }),
   descricao: z.string().trim().min(1, {
