7f192a22e5d5a370bdab805497b1aceaeea75139
Vinicyos Ferreira <vinifr987@gmail.com>
2026-06-26 12:13:29 -0300

    feat: add UploadStorageUseCase for AWS S3 client operations

---
+++ src/use-cases/storage/UploadStorageUseCase.ts
@@ -0,0 +1,77 @@
+import 'dotenv/config';
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+  DeleteObjectCommand,
+} from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { MultipartFile } from '@fastify/multipart';
+
+export class UploadStorageUseCase {
+  private s3Client: S3Client;
+  constructor() {
+    this.s3Client = new S3Client({
+      region: 'us-east-1',
+      endpoint: 'http://localhost:9000',
+      credentials: {
+        accessKeyId: process.env.MINIO_ROOT_USER || '',
+        secretAccessKey: process.env.MINIO_ROOT_PASSWORD || '',
+      },
+      forcePathStyle: true,
+    });
+  }
+  async saveDocument(file: MultipartFile) {
+    const uniqueFileName = new Date().getTime() + '-' + file.filename;
+
+    const save = new PutObjectCommand({
+      Bucket: 'documents',
+      Key: uniqueFileName,
+      Body: await file.toBuffer(),
+      ContentType: file.mimetype,
+    });
+
+    try {
+      await this.s3Client.send(save);
+      return {
+        document_key: `${uniqueFileName}`,
+      };
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  async generateSignedUrl(documentKey: string) {
+    const command = new GetObjectCommand({
+      Bucket: 'documents',
+      Key: documentKey,
+    });
+    try {
+      const signed = await getSignedUrl(this.s3Client, command, {
+        expiresIn: 3600,
+      });
+      return signed;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  async deleteDocument(documentKey: string) {
+    const command = new DeleteObjectCommand({
+      Bucket: 'documents',
+      Key: documentKey,
+    });
+
+    try {
+      await this.s3Client.send(command);
+      return {
+        message: 'Documento deletado com sucesso',
+      };
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+}
